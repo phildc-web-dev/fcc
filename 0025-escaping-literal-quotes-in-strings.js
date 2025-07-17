94286d542b58db5b5cfc06a623a3124473c24256
Phil Pickering <phildc.web.dev@gmail.com>
2025-07-17 01:11:13 +0100

    Update 0025-escaping-literal-quotes-in-strings.js

--- 0025-escaping-literal-quotes-in-strings.js
+++ 0025-escaping-literal-quotes-in-strings.js
@@ -1,6 +1,1 @@
-** start of script.js **
-
-const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
-
-** end of script.js **
-
+const myStr = "I am a \"double quoted\" string inside \"double quotes\".";
