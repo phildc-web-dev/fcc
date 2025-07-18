47069c0a401b3c7db08a2c8da009bca930f6b4f3
Phil Pickering <phildc.web.dev@gmail.com>
2025-07-18 20:07:55 +0100

    Update 0035-use-bracket-notation-to-find-the-nth-character-in-a-string.js

--- 0035-use-bracket-notation-to-find-the-nth-character-in-a-string.js
+++ 0035-use-bracket-notation-to-find-the-nth-character-in-a-string.js
@@ -1,10 +1,2 @@
-** start of script.js **
-
-// Setup
 const lastName = "Lovelace";
-
-// Only change code below this line
-const thirdLetterOfLastName = lastName[2]; // Change this line
-
-** end of script.js **
-
+const thirdLetterOfLastName = lastName[2];
