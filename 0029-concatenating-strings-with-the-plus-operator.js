02323bf480cb6502e9c9299d8d09e89e031e0885
Phil Pickering <phildc.web.dev@gmail.com>
2025-07-18 08:44:18 +0100

    Update 0029-concatenating-strings-with-the-plus-operator.js

--- 0029-concatenating-strings-with-the-plus-operator.js
+++ 0029-concatenating-strings-with-the-plus-operator.js
@@ -1,6 +1,1 @@
-** start of script.js **
-
-const myStr = "This is the start." + " This is the end."; // Change this line
-
-** end of script.js **
-
+const myStr = "This is the start." + " This is the end.";
