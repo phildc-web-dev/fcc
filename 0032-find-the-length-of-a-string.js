a830a069f73dd1a31d7adfef6198c673d857ce74
Phil Pickering <phildc.web.dev@gmail.com>
2025-07-18 20:04:25 +0100

    Update 0032-find-the-length-of-a-string.js

--- 0032-find-the-length-of-a-string.js
+++ 0032-find-the-length-of-a-string.js
@@ -1,11 +1,4 @@
-** start of script.js **
-
-// Setup
 let lastNameLength = 0;
 const lastName = "Lovelace";
 
-// Only change code below this line
 lastNameLength = lastName.length;
-
-** end of script.js **
-
