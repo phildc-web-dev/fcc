8bd861a966a45035dba5614389d65651adff291d
Phil Pickering <phildc.web.dev@gmail.com>
2025-07-19 15:59:58 +0100

    Update 0037-use-bracket-notation-to-find-the-nth-to-last-character-in-a-string.js

--- 0037-use-bracket-notation-to-find-the-nth-to-last-character-in-a-string.js
+++ 0037-use-bracket-notation-to-find-the-nth-to-last-character-in-a-string.js
@@ -1,10 +1,2 @@
-** start of script.js **
-
-// Setup
 const lastName = "Lovelace";
-
-// Only change code below this line
-const secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
-
-** end of script.js **
-
+const secondToLastLetterOfLastName = lastName[lastName.length - 2];
