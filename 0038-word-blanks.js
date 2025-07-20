57c57a18714785b864c84c09e0a9f481d259fd00
Phil Pickering <phildc.web.dev@gmail.com>
2025-07-20 13:25:07 +0100

    Update 0038-word-blanks.js

--- 0038-word-blanks.js
+++ 0038-word-blanks.js
@@ -1,13 +1,6 @@
-** start of script.js **
-
 const myNoun = "dog";
 const myAdjective = "big";
 const myVerb = "ran";
 const myAdverb = "quickly";
 
-// Only change code below this line
-const wordBlanks = "We met a " + myAdjective + " " + myNoun + ", so we " + myAdverb + " " + myVerb + " away."; // Change this line
-// Only change code above this line
-
-** end of script.js **
-
+const wordBlanks = "We met a " + myAdjective + " " + myNoun + ", so we " + myAdverb + " " + myVerb + " away.";
