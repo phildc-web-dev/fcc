643e40e1ea72b3cde20d2970fd934db6d876a098
Phil Pickering <phildc.web.dev@gmail.com>
2025-07-21 08:16:23 +0100

    Update 0046-manipulate-arrays-with-pop.js

--- 0046-manipulate-arrays-with-pop.js
+++ 0046-manipulate-arrays-with-pop.js
@@ -1,10 +1,3 @@
-** start of script.js **
-
-// Setup
 const myArray = [["John", 23], ["cat", 2]];
 
-// Only change code below this line
 const removedFromMyArray = myArray.pop();
-
-** end of script.js **
-
