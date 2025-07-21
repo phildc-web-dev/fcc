2be6ed21776f6c2b633d99d639762a3afd0089eb
Phil Pickering <phildc.web.dev@gmail.com>
2025-07-21 08:17:25 +0100

    Update 0047-manipulate-arrays-with-shift.js

--- 0047-manipulate-arrays-with-shift.js
+++ 0047-manipulate-arrays-with-shift.js
@@ -1,10 +1,3 @@
-** start of script.js **
-
-// Setup
 const myArray = [["John", 23], ["dog", 3]];
 
-// Only change code below this line
 const removedFromMyArray = myArray.shift();
-
-** end of script.js **
-
