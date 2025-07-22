39540b8c4a1b3759acdf33c2abbabe1e95672505
Phil Pickering <phildc.web.dev@gmail.com>
2025-07-22 09:28:38 +0100

    Update 0054-global-vs--local-scope-in-functions.js

--- 0054-global-vs--local-scope-in-functions.js
+++ 0054-global-vs--local-scope-in-functions.js
@@ -1,16 +1,8 @@
-** start of script.js **
-
-// Setup
 const outerWear = "T-Shirt";
 
 function myOutfit() {
-  // Only change code below this line
   const outerWear = "sweater";
-  // Only change code above this line
   return outerWear;
 }
 
 myOutfit();
-
-** end of script.js **
-
