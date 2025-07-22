dc299de2e53027e54ab80fef3c6015bd8186d440
Phil Pickering <phildc.web.dev@gmail.com>
2025-07-22 09:24:38 +0100

    Update 0052-global-scope-and-functions.js

--- 0052-global-scope-and-functions.js
+++ 0052-global-scope-and-functions.js
@@ -1,25 +1,5 @@
-** start of script.js **
-
-// Declare the myGlobal variable below this line
 const myGlobal = 10;
 
 function fun1() {
-  // Assign 5 to oopsGlobal here
   oopsGlobal = 5;
 }
-
-// Only change code above this line
-
-function fun2() {
-  let output = "";
-  if (typeof myGlobal != "undefined") {
-    output += "myGlobal: " + myGlobal;
-  }
-  if (typeof oopsGlobal != "undefined") {
-    output += " oopsGlobal: " + oopsGlobal;
-  }
-  console.log(output);
-}
-
-** end of script.js **
-
