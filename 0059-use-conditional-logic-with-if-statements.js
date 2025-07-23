e7b70cbfe0cc9161f717e78911ee43e82df0c0cf
Phil Pickering <phildc.web.dev@gmail.com>
2025-07-23 15:00:24 +0100

    Update 0059-use-conditional-logic-with-if-statements.js

--- 0059-use-conditional-logic-with-if-statements.js
+++ 0059-use-conditional-logic-with-if-statements.js
@@ -1,16 +1,6 @@
-** start of script.js **
-
 function trueOrFalse(wasThatTrue) {
-  // Only change code below this line
   if(wasThatTrue) {
     return "Yes, that was true";
   }
   return "No, that was false";
-
-
-  // Only change code above this line
-
 }
-
-** end of script.js **
-
