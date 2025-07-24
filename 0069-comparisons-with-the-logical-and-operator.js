ee3f398ed462b34de9b669737bd9a8becf2d6a3c
Phil Pickering <phildc.web.dev@gmail.com>
2025-07-24 13:25:20 +0100

    Update 0069-comparisons-with-the-logical-and-operator.js

--- 0069-comparisons-with-the-logical-and-operator.js
+++ 0069-comparisons-with-the-logical-and-operator.js
@@ -1,17 +1,10 @@
-** start of script.js **
-
 function testLogicalAnd(val) {
-  // Only change code below this line
 
   if (val >= 25 && val <= 50) {
     return "Yes"
   }
 
-  // Only change code above this line
   return "No";
 }
 
 testLogicalAnd(10);
-
-** end of script.js **
-
