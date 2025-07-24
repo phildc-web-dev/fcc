a1b415614e890aa73e2fd784cfeaba01d9190cde
Phil Pickering <phildc.web.dev@gmail.com>
2025-07-24 13:26:19 +0100

    Update 0070-comparisons-with-the-logical-or-operator.js

--- 0070-comparisons-with-the-logical-or-operator.js
+++ 0070-comparisons-with-the-logical-or-operator.js
@@ -1,17 +1,10 @@
-** start of script.js **
-
 function testLogicalOr(val) {
-  // Only change code below this line
 
   if (val < 10 || val > 20) {
     return "Outside";
   }
 
-  // Only change code above this line
   return "Inside";
 }
 
 testLogicalOr(15);
-
-** end of script.js **
-
