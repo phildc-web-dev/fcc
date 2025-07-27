5a0c4f1f1e9102d34a417fcb3b17f21031fa5e1a
Phil Pickering <phildc.web.dev@gmail.com>
2025-07-27 18:49:01 +0100

    Update 0083-build-javascript-objects.js

--- 0083-build-javascript-objects.js
+++ 0083-build-javascript-objects.js
@@ -1,13 +1,6 @@
-** start of script.js **
-
 const myDog = {
-  // Only change code below this line
   "name": "Pluto",
   "legs": 5,
   "tails": 1,
   "friends": ["Mickey", "Goofy"]
-  // Only change code above this line
 };
-
-** end of script.js **
-
